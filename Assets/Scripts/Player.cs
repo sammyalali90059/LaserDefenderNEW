79b52d53a3eeda9ced8245927c2ff6b4bee8d36b
sammyalali90059 <wwehdv9@gmail.com>
2020-11-12 22:58:44 +0100

    LaserDefnderNEW
    Had troubles with the old files so i recreated it

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    [SerializeField] float laserSpeed = 20f;
+    [SerializeField] float laserDelay = 0.3f;
+
+    [SerializeField] GameObject laserPrefab;
+    float xMin;
+    float yMin;
+    float xMax;
+    float yMax;
+    [SerializeField] float padding = 0.5f;
+    //Serialize Field makes the variable accessible from Unity Editor without making it public.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+       // StartCoroutine(PrintAndWait());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+
+    private void Move()
+    {
+        //var is used as a generic variable. VS allows us to use var and it will set its type depending on the value it will have.
+        // deltaX will be updated with the input that will happend on the x-axis, left and right
+
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        //the x-position will be updated according to the newXPost - whether left or right. Y position will be the same
+
+        transform.position = new Vector2(newXPos, newYPos);
+
+        
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        //setup the boundaries of the movement according to the camera
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+
+    }
+
+    private void Fire()
+    {
+        //if a button linked to Fire1 is pressed
+        if (Input.GetButtonDown("Fire1")) 
+        {
+            StartCoroutine(FireContinuously());
+        }
+    }
+
+    IEnumerator PrintAndWait()
+    {
+        print("Message 1 sent!");
+
+        yield return new WaitForSeconds(3f);
+
+        print("Message 2 sent!");
+    }
+    IEnumerator FireContinuously()
+    {
+        while(true)
+        {
+            /*create an instance of the laserPrefab,
+             at the position of the spaceship (this)
+            with default rotation.. */
+            GameObject laserClone = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laserClone.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+            yield return new WaitForSeconds(laserDelay);
+        
+        }
+    }
+    
+}
