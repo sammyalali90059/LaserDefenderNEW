e16c6f9f281d2da134be729320866f9d26fb88d9
sammyalali90059 <wwehdv9@gmail.com>
2020-11-20 14:26:17 +0100

    Fixed ERROR
    Enemy was not moving, Fixed now

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    //referemce to the crrent wave asset
+    //reference to the current wave asset (type wave config)
     [SerializeField] WaveConfig waveConfig;
-    // Start is called before the first frame update
 
     [SerializeField] List<Transform> wayPoints;
     [SerializeField] float enemyMoveSpeed = 2f;
 
     int wayPointIndex = 0;
+
+    // Start is called before the first frame update
     void Start()
     {
+        //current waypoints list is filled up by all of the points saved in the pathprefab specified in the
+        //current wave asset.
         wayPoints = waveConfig.GetWaypoints();
-        // we are setting the current enemy to place in the position of the first waypoint
+        //we are setting the current enemy to be placed in the position of the first waypoint
         transform.position = wayPoints[wayPointIndex].position;
     }
 
@@ -27,26 +30,35 @@ public class EnemyPathing : MonoBehaviour
 
     void EnemyMove()
     {
-        if(wayPointIndex < wayPoints.Count)
-            //if(wayPointIndex <= wayPoints.Count - 1)
+        if (wayPointIndex < wayPoints.Count)
+        //if(wayPointIndex <= wayPoints.Count - 1)
         {
-            /*
-             Since the enemy movement method wil be called every frame,the movements speed will be frame dependant. just like we did for the player, we are using Time.deltaTime to make the speed frame independendt
-            */
+            /* since the enemy movement method will be called every frame, the movement's speed
+             * will be frame dependant. Just like we did for the player, we are using Time.deltaTime
+             * to make the speed frame independant.
+             */
             var movementThisFrame = enemyMoveSpeed * Time.deltaTime;
             var targetPosition = wayPoints[wayPointIndex].position;
 
-            /*
+            /* MoveTowards is a method found in the Vector2 class and it basically moves an object
+             * from the current position to a target position/destination by returning a new
+             * position every frame to slowly move the object to the target step by step and 
+             * making it look as if it is moving along a path.
              */
 
-            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition,
+                movementThisFrame);
 
             if (transform.position == targetPosition)
-                wayPointIndex++;
+                wayPointIndex++; // wayPointIndex = wayPointIndex + 1;
         }
-
-        else
+        else // if there are no more coordinates left in the list
         {
+            /* The gameObject keyword refers to the object which contains the current script.
+             * This is important since we are going to have multiple Enemies and we need to one
+             * which one to destroy. The one which has moved along ALL the coordinates (so the one
+             * that reached this else statement) will be destroyed.
+             */
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/WaveConfig.cs
+++ Assets/Scripts/WaveConfig.cs
@@ -2,25 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* Usual scripts are used to control game objects and they are of type MonoBehaviour. This script
+ * is generated so as to create a collection of data/variables which will be required by all
+ * enemy prefabs/copies. Without creating this script (of type ScriptableObject), the same set
+ * of variables will be duplicated for EACH prefab and thus, using up extra memory. Instead, one
+ * copy of the collection of data will be stored and it can be accessed by all of the prefabs.
+ */
 
 [CreateAssetMenu(menuName = "Enemy Wave Config")]
 public class WaveConfig : ScriptableObject
 {
+    //the enemy
     [SerializeField] GameObject enemyPrefab;
 
+    //the path on which to go
     [SerializeField] GameObject pathPrefab;
 
+    //time between each spawn
     [SerializeField] float timeBetweenSpawns = 0.5f;
+
+    //include this random difference between spwans
     [SerializeField] float spawnRandomFactor = 0.3f;
 
+    //number of enemies in the wave
     [SerializeField] int numberOfEnemies = 5;
+
+    //enemy movement speed
     [SerializeField] float enemyMoveSpeed = 2f;
 
+    /* (an element is items created in a class so variable, property or method)
+     * The variables above are all set to private since unless we specify the element's access
+     * modifier, by default they will be private since this is the most secure access modifier.
+     * A private element would mean that the element can ONLY be accessed from the same class in
+     * which it was created in.
+     * The above variables will be left as private so that no other class can change their value
+     * by mistake. The variables are going to be set as Read Only since the below methods will
+     * return the variables' values and thus, can ONLY be read from the script in which the method
+     * will be called.
+     */
     public GameObject GetEnemyPrefab()
     {
         return enemyPrefab;
     }
-
     /*public GameObject GetPathPrefab()
     {
         return pathPrefab;
@@ -31,9 +54,12 @@ public class WaveConfig : ScriptableObject
         //creating an empty list to place each waypoint from the pathprefab inside this list
         List<Transform> waypoints = new List<Transform>();
 
-        //Syntax for a foreach is foreach item(indicate type of item and give it a temp name) in collection
-        foreach(Transform child in pathPrefab.transform)
+        //Syntax for a Foreach is foreach item (indicate type of item and give it a temp name) in collection
+        foreach (Transform child in pathPrefab.transform)
         {
+            /* Go through all of the pathprefabs children (which are the waypoints for the path) and place
+             * each one in the waypoints list.
+             */
             waypoints.Add(child);
         }
 
@@ -44,7 +70,7 @@ public class WaveConfig : ScriptableObject
     {
         return timeBetweenSpawns;
     }
-    public float GetRandomSpawnRandomFactor()
+    public float GetSpawnRandomFactor()
     {
         return spawnRandomFactor;
     }
